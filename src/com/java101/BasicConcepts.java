61786a4caa6930dcb44410c74c11b521d57b65e7
isildakli <isildakli.mc@gmail.com>
2022-03-02 01:21:28 +0300

    second class was created

--- src/com/java101/BasicConcepts.java
+++ src/com/java101/BasicConcepts.java
@@ -1,4 +1,4 @@
-package com.basics;
+package com.java101;
 import java.util.Scanner;
 
 public class BasicConcepts {
---
+++ src/com/java101/ConditionalOperations.java
@@ -0,0 +1,96 @@
+package com.java101;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class ConditionalOperations {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Calculator -->
+
+//        int result = 0;
+//        System.out.print("first number?");
+//        int first = input.nextInt();
+//
+//        System.out.print("second number?");
+//        int second = input.nextInt();
+//
+//        System.out.print("which operation?");
+//        String operation = input.next();
+//
+//        if (operation == "+")
+//            result = first+second;
+//        if (operation == "-")
+//            result = first-second;
+//        if (operation == "*")
+//            result = first*second;
+//        if (operation == "/")
+//            result = first/second;
+//
+//        System.out.println("Result is : " + result);
+
+        // User login -->
+
+//        String username, password = "123456";
+//
+//
+//        System.out.print("username?");
+//        username = input.nextLine();
+//
+//        System.out.print("password?");
+//        String password_user = input.nextLine();
+//
+//        if (!Objects.equals(password, password_user)){
+//            System.out.println("password is wrong ,do you want to change it?");
+//            System.out.print("password?");
+//            String password_new = input.nextLine();
+//            if (password != password_new && password_new != password_user){
+//                System.out.print("password is changed successfully");
+//            }
+//            else
+//                System.out.print("password is not changed!");
+//
+//        }
+
+        //  Pass or Fail --->
+
+//        int phy, math, chem, tur, mus,total=0;
+//
+//
+//        System.out.print("phy grade?");
+//        phy = input.nextInt();
+//        if (phy < 101 && phy > -1)
+//            total = total + phy;
+//
+//        System.out.print("chem grade?");
+//        chem = input.nextInt();
+//        if (chem < 101 && chem > -1)
+//            total = total + chem;
+//
+//        System.out.print("tur grade?");
+//        tur = input.nextInt();
+//        if (tur < 101 && tur > -1)
+//            total = total + tur;
+//
+//        System.out.print("math grade?");
+//        math = input.nextInt();
+//        if (math < 101 && math > -1)
+//            total = total + math;
+//
+//        System.out.print("mus grade?");
+//        mus = input.nextInt();
+//        if (mus < 101 && mus > -1)
+//            total = total + mus;
+//
+//        double result = total/6.0;
+//
+//        String message = (result >= 60) ? " you pass" : " you fail";
+//        System.out.print(message);
+
+
+
+    }
+}
