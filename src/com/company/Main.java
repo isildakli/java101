954dbbad18a9b18ff84edaf4ebbd770639a6be15
isildakli <isildakli.mc@gmail.com>
2022-03-01 10:35:16 +0300

    commit for all practices

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,76 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Average Calculation
-        int phy, math, chem, tur, his, mus;
-
-
         Scanner input = new Scanner(System.in);
-        System.out.print("phy grade?");
-        phy = input.nextInt();
 
-        System.out.print("chem grade?");
-        chem = input.nextInt();
+        // Average Calculation --->
 
-        System.out.print("tur grade?");
-        tur = input.nextInt();
+//        int phy, math, chem, tur, his, mus;
+//
+//
+//        System.out.print("phy grade?");
+//        phy = input.nextInt();
+//
+//        System.out.print("chem grade?");
+//        chem = input.nextInt();
+//
+//        System.out.print("tur grade?");
+//        tur = input.nextInt();
+//
+//        System.out.print("math grade?");
+//        math = input.nextInt();
+//
+//        System.out.print("his grade?");
+//        his = input.nextInt();
+//
+//        System.out.print("mus grade?");
+//        mus = input.nextInt();
+//
+//        double result = (phy +chem +tur +math +his +mus)/6.0;
+//
+//        String message = (result >= 60) ? " you pass" : " you fail";
+//        System.out.print(message);
 
-        System.out.print("math grade?");
-        math = input.nextInt();
+        // KDV Tax --->
 
-        System.out.print("his grade?");
-        his = input.nextInt();
+//        double amount, KDVratio_1=0.18, KDVratio_2=0.08 ;
+//        System.out.print("What is fee? ");
+//        amount =input.nextDouble();
+//        double KDVratio = (amount <= 1000) ? KDVratio_1 : KDVratio_2;
+//        double KDVtax = amount * KDVratio;
+//        double total = amount + KDVtax;
+//        System.out.println(total);
 
-        System.out.print("mus grade?");
-        mus = input.nextInt();
+        // Triangle Area Calculation --->
 
-        double result = (phy +chem +tur +math +his +mus)/6.0;
+//        System.out.print("What is  first length? ");
+//        double f_len =input.nextDouble();
+//        System.out.print("What is second length? ");
+//        double s_len =input.nextDouble();
+//        System.out.print("What is third length? ");
+//        double t_len =input.nextDouble();
+//        double u = ((f_len+s_len+t_len)/2);
+//        double area = u*(u-f_len)*(u-s_len)*(u-t_len);
+//        System.out.println(area);
 
-        String message = (result >= 60) ? " you pass" : " you fail";
-        System.out.print(message);
+        // Calculation of Taxi Fee --->
+
+//        System.out.print("What is the distance traveled? (in Km) ");
+//        double distance =input.nextDouble();
+//        double fee = ((distance * 2.20) + 10 <= 20) ? 20 : (distance * 2.20) + 10;
+//        System.out.println("Taxi cost is : "+ fee);
+
+        // Calculation Area of a part of Disc --->
+
+//        double pi = 3.14;
+//        System.out.print("What is the center angle");
+//        double angle =input.nextDouble();
+//        System.out.print("What is the radius");
+//        double radius =input.nextDouble();
+//        double area =(pi * (radius*radius) * angle) / 360;
+//        System.out.print("Area : "+ area);
+
+
 
     }
 }
