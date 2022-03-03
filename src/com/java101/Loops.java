2397e7eff114d30e55500a449a7ccf977cd94b4f
isildakli <isildakli.mc@gmail.com>
2022-03-03 14:35:58 +0300

    loops class are finished

--- src/com/java101/Loops.java
+++ src/com/java101/Loops.java
@@ -1,5 +1,6 @@
 package com.java101;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Loops {
@@ -127,7 +128,154 @@ public class Loops {
 //            System.out.println(" ");
 //        }
 
+        // Bank Application
 
+//        String userName, password;
+//        int right = 3;
+//        long balance = 1500;
+//        int select;
+//        while (right > 0) {
+//            System.out.print("Kullanıcı Adınız :");
+//            userName = input.nextLine();
+//            System.out.print("Parolanız : ");
+//            password = input.nextLine();
+//
+//            if (userName.equals("patika") && password.equals("dev123")) {
+//                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+//                do {
+//                    System.out.println("1-Para yatırma\n" +
+//                            "2-Para Çekme\n" +
+//                            "3-Bakiye Sorgula\n" +
+//                            "4-Çıkış Yap");
+//                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+//                    select = input.nextInt();
+//                    switch (select){
+//                        case 1: {
+//                            System.out.print("Para miktarı : ");
+//                            int price = input.nextInt();
+//                            balance += price;
+//                            break;}
+//                        case 2: {
+//                            System.out.print("Para miktarı : ");
+//                            int price = input.nextInt();
+//                            if (price > balance) {
+//                                System.out.println("Bakiye yetersiz.");
+//                            }
+//                            else {
+//                                balance -= price;
+//                            }
+//                            break;}
+//                        case 3: System.out.println("Bakiyeniz : " + balance); break;
+//                    }
+//                } while (select != 4);
+//                System.out.println("Tekrar görüşmek üzere.");
+//                break;
+//            }
+//            else {
+//                right--;
+//                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+//                if (right == 0) {
+//                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+//                } else {
+//                    System.out.println("Kalan Hakkınız : " + right);
+//                }
+//            }
+//        }
+
+        //  The Biggest Common Denominator and The Smallest Common Mutiple
+
+//        System.out.print("write the first number : ");
+//        int number1 = input.nextInt();
+//        System.out.print("write the second number : ");
+//        int number2 = input.nextInt();
+//        int smaller,ebob = 0,ekok,c = 1 ;
+//
+//        if (number1 < number2)
+//            smaller = number1;
+//        else
+//            smaller = number2;
+//
+//        while (c <= smaller){
+//            if (number1 % c == 0 && number2 % c == 0){
+//                ebob = c;
+//            }
+//            c++;
+//        }
+//        ekok = (number1 * number2)/ebob ;
+//
+//        System.out.println("The Biggest Common Denominator :"+ ebob);
+//        System.out.print("The Smallest Common Mutiple :" +ekok);
+
+
+        // The Biggest and The Smallest Number in a Set
+
+//        System.out.print("How many number will you write? : ");
+//        int number = input.nextInt();
+//        int n1,n2 = 0,biggest = Integer.MIN_VALUE,smallest = Integer.MAX_VALUE;
+//
+//        System.out.print("Enter a number ");
+//        n1 = input.nextInt();
+//        for (int i = 1; i < number; i++){
+//            System.out.print("Enter a number ");
+//            n2 = input.nextInt();
+//            if (n1 > n2) {
+//                biggest = n1;
+//                if (n2 < smallest){ smallest  = n2;}
+//            }
+//            else {
+//                biggest = n2;
+//                if (n1 < smallest){ smallest  = n1;}
+//            }
+//        }
+//        System.out.println("the smallest : " + smallest);
+//        System.out.print("the biggest : " + biggest);
+
+
+        // The Great Number
+
+//        System.out.print(" Write a number ..");
+//        int number = input.nextInt();
+//        int sum = 0;
+//
+//        for (int i = 1; i < number; i++){
+//            if (number % i == 0){
+//                sum = sum + i;
+//            }
+//        }
+//
+//        if (sum == number)
+//            System.out.print(" it is GREAT");
+//        else
+//            System.out.print(" it is not great");
+
+
+        // Prime Number (1-100)
+
+//        boolean situation ;
+//
+//        for (int i = 2; i < 101; i++){
+//            situation = true ;
+//            for (int j = 2; j < i; j++){
+//                if (i % j == 0 && !Objects.equals(i,2))
+//                    situation = false;
+//            }
+//            if (situation == true)
+//                System.out.print(i + "--");
+//        }
+
+        // Fibonacci Series
+
+//        System.out.print(" Write series length");
+//        int number = input.nextInt();
+//        int n1 = 0,n2 = 1,n3;
+//
+//        for(int i=2;i<number;++i)
+//        {
+//            n3= n1+ n2;
+//            System.out.print(" "+n3);
+//            n1=n2;
+//            n2=n3;
+//        }
 
 
     }
